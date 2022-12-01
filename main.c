db48f208e6e09136a85420a4df2a4889165a5e4b
camaronwilliams <103055932+camaronwilliams@users.noreply.github.com>
2022-12-01 16:27:48 -0500

    Finished

--- main.c
+++ main.c
@@ -1,129 +1,83 @@
+//
+// Created by camaron.williams on 11/29/2022.
+//
 #include<stdio.h>
+#include<stdlib.h>
 #include<math.h>
-#include <stdlib.h>
-#define MAX 100
 
-struct Point
+typedef struct Point
 {
     int x;
     int y;
-};
+}Point;
 
-struct Line
+typedef struct Line
 {
     struct Point a;
     struct Point b;
     float length;
-};
+}Line;
+
+struct Line * readFile(char file_name[], int *n);
+void computeLengths(struct Line *lines, int n);
+void saveLengths(struct Line *lines, int n);
+void printStats(struct Line *lines, int n);
+
+int main(int argc, char *argv[]){
+    int n;
+    Line *lines=readFile(argv[1],&n);
+    computeLengths(lines,n);
+    saveLengths(lines,n);
+    printStats(lines,n);
+    return 0;
+}
 
 struct Line * readFile(char file_name[], int *n)
 {
-    int i, j, size;
-    FILE *fp = fopen(file_name, "r");
-    if(fp == NULL)
-    {
-        printf("Error opening file.\n");
-        return NULL;
-    }
-
-    fscanf(fp, "%d", &size);
-    struct Line *lines = (struct Line *) malloc(sizeof(struct Line) * size);
-
-    printf("Array of size %d created\n", size);
-    for(i = 0; i < size; i++)
-    {
-        fscanf(fp, "%d %d %d %d", &(lines[i].a.x), &(lines[i].a.y), &(lines[i].b.x), &(lines[i].b.y));
-    }
-
-    *n = size;
-
+    FILE *fp=fopen(file_name,"r");
+    fscanf(fp,"%d", n);
+    Line *lines=(Line *)malloc(sizeof(Line)*(*n));
+    printf("Array of size %d created\n",*n);
+    for(int i=0;i<*n; i++)
+    fscanf(fp,"%d %d %d %d",&lines[i].a.x,&lines[i].a.y,&lines[i].b.x,&lines[i].b.y);
     printf("Data read\n");
-
-    fclose(fp);
-
     return lines;
 }
 
 void computeLengths(struct Line *lines, int n)
 {
-    int i;
-
     printf("Computing lengths...\n");
-
-    for(i = 0; i < n; i++)
+    int i;
+    for(i=0;i<n;i++)
     {
-
-        lines[i].length = sqrt(pow((lines[i].a.x - lines[i].b.x), 2) + pow((lines[i].a.y - lines[i].b.y), 2));
+        lines[i].length=sqrt(pow(lines[i].a.x-lines[i].b.x,2)+pow(lines[i].a.y-lines[i].b.y,2));
     }
-
     printf("Length computation completed\n");
 }
+
 void saveLengths(struct Line* lines, int n)
 {
-    int i;
-
-    // open file lengths_LastName_firstName.txt to write
-    FILE *fp = fopen("lengths_LastName_firstName.txt", "w");
-
-    // check if file opened successfully
-    if(fp == NULL)
-    {
-        printf("Error opening file.\n");
-        return;
-    }
-
     printf("Saving lengths...\n");
-    for(i = 0; i < n; i++)
-    {
-        fprintf(fp, "%.1f\n", lines[i].length);
-    }
-
+    FILE *fp;
+    fp=fopen("lengths_Williams_Camaron.txt","w");
+    for(int i=0;i<n;i++)
+        fprintf(fp,"%.1f\n",lines[i].length);
     fclose(fp);
 }
+
 void printStats(struct Line* lines, int n)
 {
+    float max=lines[0].length;
+    float min=lines[0].length;
+    float avg=0;
     int i;
-    float max, min, avg;
-    max = min = avg = lines[0].length;
-
-    for(i = 1; i < n; i++)
+    for(i=0;i<n;i++)
     {
-        if(max < lines[i].length)
-        {
-            max = lines[i].length;
-        }
-
-        if(min > lines[i].length)
-        {
-            min = lines[i].length;
-        }
-
+        if(lines[i].length>max)
+            max=lines[i].length;
+        if(lines[i].length<min)
+            min=lines[i].length;
         avg += lines[i].length;
     }
-
-    avg /= n;
-
-    printf("Line of max length: %.1f\n", max);
-    printf("Line with min length: %.1f\n", min);
-    printf("Average line length: %.1f\n", avg);
-}
-
-int main(int argc, char* argv[])
-{
-    int i, n;
-    if(argc != 2)
-    {
-        printf("Error! Please enter filename as command-line argument.\n");
-        return 1;
-    }
-
-    struct Line *lines = readFile(argv[1], &n);
-
-    computeLengths(lines, n);
-
-    saveLengths(lines, n);
-
-    printStats(lines, n);
-
-    return 0;
+    printf("Line of max length: %.1f\nLine with min length: %.1f\nAverage line length: %.1f\n", max, min, avg/n);
 }
